7f84228e363dd78a3653a77d5fce8dc5463a8ca5
SYCO-Climbing <info@sycoclimbing.org>
2025-08-24 20:47:14 -0400

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,55 @@
+<html>
+<body>
+
+<?php
+if($_POST['formSubmit'] == "Submit") 
+{
+  $errorMessage = "";
+
+  if(empty($_POST['fname'])) 
+  {
+    $errorMessage .= "<li>Please enter your first name!</li>";
+  }
+  if(empty($_POST['lname'])) 
+  {
+    $errorMessage .= "<li>Please enter your last name!</li>";
+  }
+  if(empty($_POST['email'])) 
+  {
+    $errorMessage .= "<li>Please enter your email!</li>";
+  }
+
+  $varFirstName = $_POST['fname'];
+  $varLastName = $_POST['lname'];
+  $varEmail = $_POST['email'];
+  $varMessage = $_POST['message']
+
+  if(!empty($errorMessage)) 
+  {
+    echo("<p>There was an error with your form:</p>\n");
+    echo("<ul>" . $errorMessage . "</ul>\n");
+  } 
+
+}
+   
+?>
+
+<?php
+if($errorMessage != "") 
+{
+  echo("<p>There was an error:</p>\n");
+  echo("<ul>" . $errorMessage . "</ul>\n");
+} 
+else 
+{
+  $fs = fopen("mydata.csv","a");
+  fwrite($fs,$varFirstName . ", " . $varLastName . ", " . $varEmail . ", " . $varMessage . "\n");
+  fclose($fs);
+
+  header("Location: ../build/index2.html");
+  exit;
+}
+?>
+
+</body>
+</html>
